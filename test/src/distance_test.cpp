33a789ef562b111c4a3cd73279c4e25f3c4b7b82
Eric Wasylishen <ewasylishen@gmail.com>
2020-08-10 19:52:02 -0600

    20: refactor tests

--- test/src/distance_test.cpp
+++ test/src/distance_test.cpp
@@ -66,7 +66,9 @@ namespace vm {
     }
 
     template <class A, class B>
-    static void assert_line_distance_squared_invariants(const line_distance<float>& linedist, const A& lhs, const B& rhs) {
+    static void assert_line_distance_invariants(const A& lhs, const B& rhs) {
+        const auto linedist = squared_distance(lhs, rhs);
+
         const vec3f lhsPoint = point_at_distance(lhs, linedist.position1);
         const vec3f rhsPoint = point_at_distance(rhs, linedist.position2);
         const float dist = squared_distance(lhsPoint, rhsPoint);
@@ -75,12 +77,11 @@ namespace vm {
     }
 
     template <class A, class B>
-    static void assert_line_distance_invariants(const line_distance<float>& linedist, const A& lhs, const B& rhs) {
-        const vec3f lhsPoint = point_at_distance(lhs, linedist.position1);
-        const vec3f rhsPoint = point_at_distance(rhs, linedist.position2);
-        const float dist = distance(lhsPoint, rhsPoint);
-
-        ASSERT_NEAR(dist, linedist.distance, constants<float>::almost_zero());
+    static void line_distance_extra_tests(const A& lhs, const B& rhs) {
+        assert_line_distance_invariants(lhs, rhs);
+        assert_line_distance_invariants(lhs.transform(mat4x4f::mirror_x()), rhs.transform(mat4x4f::mirror_x()));
+        assert_line_distance_invariants(lhs.transform(mat4x4f::mirror_y()), rhs.transform(mat4x4f::mirror_y()));
+        assert_line_distance_invariants(lhs.transform(mat4x4f::mirror_z()), rhs.transform(mat4x4f::mirror_z()));
     }
 
     TEST(distance_test, distance_ray_segment) {
@@ -95,7 +96,7 @@ namespace vm {
         ASSERT_FLOAT_EQ(0.0f, segDist.position1);
         ASSERT_FLOAT_EQ(0.0f, segDist.distance);
         ASSERT_FLOAT_EQ(0.0f, segDist.position2);
-        assert_line_distance_squared_invariants(segDist, ray, seg);
+        line_distance_extra_tests(ray, seg);
 
         // segment parallel to ray with XY offset
         seg = segment3f(vec3f(1.0f, 1.0f, 0.0f), vec3f(1.0f, 1.0f, 1.0f));
@@ -104,7 +105,7 @@ namespace vm {
         ASSERT_FLOAT_EQ(0.0f, segDist.position1);
         ASSERT_FLOAT_EQ(2.0f, segDist.distance);
         ASSERT_FLOAT_EQ(0.0f, segDist.position2);
-        assert_line_distance_squared_invariants(segDist, ray, seg);
+        line_distance_extra_tests(ray, seg);
 
         // segment parallel, in front of ray
         seg = segment3f(vec3f(1.0f, 1.0f, 5.0f), vec3f(1.0f, 1.0f, 6.0f));
@@ -113,12 +114,7 @@ namespace vm {
         ASSERT_FLOAT_EQ(5.0f, segDist.position1);
         ASSERT_FLOAT_EQ(2.0f, segDist.distance);
         ASSERT_FLOAT_EQ(0.0f, segDist.position2);
-        assert_line_distance_squared_invariants(segDist, ray, seg);
-
-        // flip previous test case
-        seg = seg.transform(mat4x4f::mirror_z());
-        segDist = squared_distance(ray.transform(mat4x4f::mirror_z()), seg);
-        assert_line_distance_squared_invariants(segDist, ray.transform(mat4x4f::mirror_z()), seg);
+        line_distance_extra_tests(ray, seg);
 
         // segment parallel, behind ray
         seg = segment3f(vec3f(1.0f, 1.0f, -2.0f), vec3f(1.0f, 1.0f, -1.0f));
@@ -127,12 +123,7 @@ namespace vm {
         ASSERT_FLOAT_EQ(0.0f, segDist.position1);
         ASSERT_FLOAT_EQ(3.0f, segDist.distance);
         ASSERT_FLOAT_EQ(1.0f, segDist.position2);
-        assert_line_distance_squared_invariants(segDist, ray, seg);
-
-        // flip previous test case
-        seg = seg.transform(mat4x4f::mirror_z());
-        segDist = squared_distance(ray.transform(mat4x4f::mirror_z()), seg);
-        assert_line_distance_squared_invariants(segDist, ray.transform(mat4x4f::mirror_z()), seg);
+        line_distance_extra_tests(ray, seg);
 
         // segment parallel, in front of ray, degenerate segment
         seg = segment3f(vec3f(1.0f, 1.0f, 5.0f), vec3f(1.0f, 1.0f, 5.0f));
@@ -141,7 +132,7 @@ namespace vm {
         ASSERT_FLOAT_EQ(5.0f, segDist.position1);
         ASSERT_FLOAT_EQ(2.0f, segDist.distance);
         ASSERT_FLOAT_EQ(0.0f, segDist.position2);
-        //assert_line_distance_squared_invariants(segDist, ray, seg); // can't do this check on a degenerate segment
+        //assert_line_distance_squared_invariants(ray, seg); // can't do this check on a degenerate segment
 
         // segment parallel, behind ray, degenerate segment
         seg = segment3f(vec3f(1.0f, 1.0f, -1.0f), vec3f(1.0f, 1.0f, -1.0f));
@@ -150,7 +141,7 @@ namespace vm {
         ASSERT_FLOAT_EQ(0.0f, segDist.position1);
         ASSERT_FLOAT_EQ(3.0f, segDist.distance);
         ASSERT_FLOAT_EQ(0.0f, segDist.position2);
-        //assert_line_distance_squared_invariants(segDist, ray, seg); // can't do this check on a degenerate segment
+        //assert_line_distance_squared_invariants(ray, seg); // can't do this check on a degenerate segment
 
         // segment perpendicular to ray
         //   R = ray
@@ -170,7 +161,7 @@ namespace vm {
         ASSERT_FLOAT_EQ(0.0f, segDist.position1);        // the ray origin is the closest point on R
         ASSERT_FLOAT_EQ(0.5f, segDist.distance);         // R to c distance, squared
         ASSERT_FLOAT_EQ(0.70710677f, segDist.position2); // s to c distance
-        assert_line_distance_squared_invariants(segDist, ray, seg);
+        line_distance_extra_tests(ray, seg);
 
         // same as previous, but segment is below ray start
         seg = segment3f(vec3f(1.0f, 0.0f, -1.0f), vec3f(0.0f, 1.0f, -1.0f));
@@ -179,7 +170,7 @@ namespace vm {
         ASSERT_FLOAT_EQ(0.0f, segDist.position1);        // the ray origin is the closest point on R
         ASSERT_FLOAT_EQ(1.5f, segDist.distance);         // R to c distance, squared
         ASSERT_FLOAT_EQ(0.70710677f, segDist.position2); // s to c distance
-        assert_line_distance_squared_invariants(segDist, ray, seg);
+        line_distance_extra_tests(ray, seg);
 
         seg = segment3f(vec3f(1.0f, 0.0f, 0.0f), vec3f(2.0f, -1.0f, 0.0f));
         segDist = squared_distance(ray, seg);
@@ -187,7 +178,7 @@ namespace vm {
         ASSERT_FLOAT_EQ(0.0f, segDist.position1);
         ASSERT_FLOAT_EQ(1.0f, segDist.distance);
         ASSERT_FLOAT_EQ(0.0f, segDist.position2);
-        assert_line_distance_squared_invariants(segDist, ray, seg);
+        line_distance_extra_tests(ray, seg);
 
         seg = segment3f(vec3f(-1.0f, 1.5f, 2.0f), vec3f(+1.0f, 1.5f, 2.0f));
         segDist = distance(ray, seg);
@@ -195,7 +186,7 @@ namespace vm {
         ASSERT_FLOAT_EQ(2.0f, segDist.position1);
         ASSERT_FLOAT_EQ(1.5f, segDist.distance);
         ASSERT_FLOAT_EQ(1.0f, segDist.position2);
-        assert_line_distance_invariants(segDist, ray, seg);
+        line_distance_extra_tests(ray, seg);
     }
 
     TEST(distance_test, distance_ray_ray) {
@@ -207,7 +198,7 @@ namespace vm {
         CER_ASSERT_NEAR(0.0f, segDist1.position1, 0.001f)
         CER_ASSERT_NEAR(0.0f, segDist1.distance, 0.001f)
         CER_ASSERT_NEAR(0.0f, segDist1.position2, 0.001f)
-        assert_line_distance_squared_invariants(segDist1, ray1, ray1);
+        line_distance_extra_tests(ray1, ray1);
 
         // parallel, XY offset
         constexpr auto segDist2Ray = ray3f(vec3f(1.0f, 1.0, 0.0f), vec3f::pos_z());
@@ -216,7 +207,7 @@ namespace vm {
         CER_ASSERT_NEAR(0.0f, segDist2.position1, 0.001f)
         CER_ASSERT_NEAR(2.0f, segDist2.distance, 0.001f)
         CER_ASSERT_NEAR(0.0f, segDist2.position2, 0.001f)
-        assert_line_distance_squared_invariants(segDist2, ray1, segDist2Ray);
+        line_distance_extra_tests(ray1, segDist2Ray);
 
         constexpr auto segDist3Ray = ray3f(vec3f(1.0f, 1.0f, 0.0f), normalize_c(vec3f(1.0f, 1.0f, 1.0f)));
         constexpr auto segDist3 = squared_distance(ray1, segDist3Ray);
@@ -224,7 +215,7 @@ namespace vm {
         CER_ASSERT_NEAR(0.0f, segDist3.position1, 0.001f)
         CER_ASSERT_NEAR(2.0f, segDist3.distance, 0.001f)
         CER_ASSERT_NEAR(0.0f, segDist3.position2, 0.001f)
-        assert_line_distance_squared_invariants(segDist3, ray1, segDist3Ray);
+        line_distance_extra_tests(ray1, segDist3Ray);
 
         constexpr auto segDist4Ray = ray3f(vec3f(1.0f, 1.0f, 0.0f), normalize_c(vec3f(-1.0f, -1.0f, +1.0f)));
         constexpr auto segDist4 = squared_distance(ray1, segDist4Ray);
@@ -232,7 +223,7 @@ namespace vm {
         CER_ASSERT_NEAR(1.0f, segDist4.position1, 0.001f)
         CER_ASSERT_NEAR(0.0f, segDist4.distance, 0.001f)
         CER_ASSERT_NEAR(length(vec3f(1.0f, 1.0f, 1.0f)), segDist4.position2, 0.001f)
-        assert_line_distance_squared_invariants(segDist4, ray1, segDist4Ray);
+        line_distance_extra_tests(ray1, segDist4Ray);
 
         constexpr auto segDist5Ray = ray3f(vec3f(1.0f, 1.0f, 0.0f), normalize_c(vec3f(-1.0f, 0.0f, +1.0f)));
         constexpr auto segDist5 = squared_distance(ray1, segDist5Ray);
@@ -240,7 +231,7 @@ namespace vm {
         CER_ASSERT_NEAR(1.0f, segDist5.position1, 0.001f)
         CER_ASSERT_NEAR(1.0f, segDist5.distance, 0.001f)
         CER_ASSERT_NEAR(length(vec3f(1.0f, 0.0f, 1.0f)), segDist5.position2, 0.001f)
-        assert_line_distance_squared_invariants(segDist5, ray1, segDist5Ray);
+        line_distance_extra_tests(ray1, segDist5Ray);
 
         // parallel, second ray is in front
         constexpr auto segDist6Ray = ray3f(vec3f(1.0f, 1.0, 1.0f), vec3f::pos_z());
@@ -249,7 +240,7 @@ namespace vm {
         CER_ASSERT_NEAR(1.0f, segDist6.position1, 0.001f)
         CER_ASSERT_NEAR(2.0f, segDist6.distance, 0.001f)
         CER_ASSERT_NEAR(0.0f, segDist6.position2, 0.001f)
-        assert_line_distance_squared_invariants(segDist6, ray1, segDist6Ray);
+        line_distance_extra_tests(ray1, segDist6Ray);
 
         // parallel, second ray is behind
         constexpr auto segDist7Ray = ray3f(vec3f(1.0f, 1.0, -1.0f), vec3f::pos_z());
@@ -258,7 +249,7 @@ namespace vm {
         CER_ASSERT_NEAR(0.0f, segDist7.position1, 0.001f)
         CER_ASSERT_NEAR(2.0f, segDist7.distance, 0.001f)
         CER_ASSERT_NEAR(1.0f, segDist7.position2, 0.001f)
-        assert_line_distance_squared_invariants(segDist7, ray1, segDist7Ray);
+        line_distance_extra_tests(ray1, segDist7Ray);
     }
 
     TEST(distance_test, distance_ray_line) {
@@ -270,7 +261,7 @@ namespace vm {
         CER_ASSERT_FLOAT_EQ(0.0f, segDist1.position1)
         CER_ASSERT_FLOAT_EQ(0.0f, segDist1.distance)
         CER_ASSERT_FLOAT_EQ(0.0f, segDist1.position2)
-        assert_line_distance_squared_invariants(segDist1, ray, segDist1Line);
+        line_distance_extra_tests(ray, segDist1Line);
 
         constexpr auto segDist2Line = line3f(vec3f(1.0f, 1.0f, 0.0f), vec3f::pos_z());
         constexpr auto segDist2 = squared_distance(ray, segDist2Line);
@@ -278,7 +269,7 @@ namespace vm {
         CER_ASSERT_FLOAT_EQ(0.0f, segDist2.position1)
         CER_ASSERT_FLOAT_EQ(2.0f, segDist2.distance)
         CER_ASSERT_FLOAT_EQ(0.0f, segDist2.position2)
-        assert_line_distance_squared_invariants(segDist2, ray, segDist2Line);
+        line_distance_extra_tests(ray, segDist2Line);
 
         constexpr auto segDist3Line = line3f(vec3f(1.0f, 0.0f, 0.0f), normalize_c(vec3f(-1.0f, 1.0f, 0.0f)));
         constexpr auto segDist3 = squared_distance(ray, segDist3Line);
@@ -286,7 +277,7 @@ namespace vm {
         CER_ASSERT_FLOAT_EQ(0.0f, segDist3.position1)
         CER_ASSERT_FLOAT_EQ(0.5f, segDist3.distance)
         CER_ASSERT_FLOAT_EQ(sqrt_c(2.0f) / 2.0f, segDist3.position2)
-        assert_line_distance_squared_invariants(segDist3, ray, segDist3Line);
+        line_distance_extra_tests(ray, segDist3Line);
 
         constexpr auto segDist4Line = line3f(vec3f(1.0f, 0.0f, 0.0f), normalize_c(vec3f(1.0f, -1.0f, 0.0f)));
         constexpr auto segDist4 = squared_distance(ray, segDist4Line);
@@ -294,7 +285,7 @@ namespace vm {
         CER_ASSERT_FLOAT_EQ(0.0f, segDist4.position1)
         CER_ASSERT_FLOAT_EQ(0.5f, segDist4.distance)
         CER_ASSERT_FLOAT_EQ(-sqrt_c(2.0f) / 2.0f, segDist4.position2)
-        assert_line_distance_squared_invariants(segDist4, ray, segDist4Line);
+        line_distance_extra_tests(ray, segDist4Line);
 
         // parallel, ray is in front of line
         constexpr auto segDist5Line = line3f(vec3f(1.0f, 1.0f, -1.0f), vec3f::pos_z());
@@ -303,7 +294,7 @@ namespace vm {
         CER_ASSERT_FLOAT_EQ(0.0f, segDist5.position1)
         CER_ASSERT_FLOAT_EQ(2.0f, segDist5.distance)
         CER_ASSERT_FLOAT_EQ(1.0f, segDist5.position2) // we use the ray origin as the closest point, so this is the corresponding position on the line
-        assert_line_distance_squared_invariants(segDist5, ray, segDist5Line);
+        line_distance_extra_tests(ray, segDist5Line);
 
         // parallel, ray is behind line
         constexpr auto segDist6Line = line3f(vec3f(1.0f, 1.0f, 1.0f), vec3f::pos_z());
@@ -312,6 +303,6 @@ namespace vm {
         CER_ASSERT_FLOAT_EQ(0.0f, segDist6.position1)
         CER_ASSERT_FLOAT_EQ(2.0f, segDist6.distance)
         CER_ASSERT_FLOAT_EQ(-1.0f, segDist6.position2) // we use the ray origin as the closest point, so this is the corresponding position on the line
-        assert_line_distance_squared_invariants(segDist6, ray, segDist6Line);
+        line_distance_extra_tests(ray, segDist6Line);
     }
 }
